c16b3d4273b7c6e080de229b6f019460027b2d52
mariacjones1 <>
2023-11-14 16:26:53 +0000

    Add following_id, like_id and comment_like_id to profiles, posts and comments serializers respectively

--- comments/serializers.py
+++ comments/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Comment
+from comment_likes.models import CommentLike
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -7,16 +8,26 @@ class CommentSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.id')
     profile_image = serializers.ReadOnlyField(source='owner.image.url')
+    comment_like_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_comment_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            comment_like = CommentLike.objects.filter(
+                owner=user, comment=obj
+            ).first()
+            return comment_like.id if comment_like else None
+        return None
+
     class Meta:
         model = Comment
         fields = [
-            'id', 'owner', 'post', 'created_at', 'updated_at', 'content', 'is_owner',
-            'profile_id', 'profile_image'
+            'id', 'owner', 'post', 'created_at', 'updated_at', 'content',
+            'is_owner', 'profile_id', 'profile_image', 'comment_like_id'
         ]
 
 
