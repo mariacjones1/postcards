9d2ecf4da0b3131be0bb3408c5f7d17c4c2a5737
mariacjones1 <>
2023-11-25 13:34:13 +0000

    Create ProfileDataContext to replace code in PopularProfiles and TrendingProfiles components

--- frontend/src/pages/profiles/TrendingProfiles.js
+++ frontend/src/pages/profiles/TrendingProfiles.js
@@ -1,36 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../../App.module.css";
 import Asset from "../../components/Asset";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useProfileData } from '../../contexts/ProfileDataContext';
 import Profile from './Profile';
 
 const TrendingProfiles = () => {
-    const [profileData, setProfileData] = useState({
-        pageProfile: { results: [] },
-        trendingProfiles: { results: [] },
-    });
-    const { trendingProfiles } = profileData;
-    const currentUser = useCurrentUser();
-
-    useEffect(() => {
-        const handleMount = async () => {
-            try {
-                const { data } = await axiosReq.get(
-                    "/profiles/?ordering=-recent_followers_count"
-                );
-                setProfileData((prevState) => ({
-                    ...prevState,
-                    trendingProfiles: data,
-                }));
-            } catch (err) {
-                console.log(err);
-            }
-        };
-
-        handleMount();
-    }, [currentUser]);
+    const { trendingProfiles } = useProfileData();
 
     return (
         <Container className={appStyles.Content}>
