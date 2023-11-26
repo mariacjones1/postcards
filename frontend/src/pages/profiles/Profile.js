ee7a07935a21ae897f488ae2fa71d431ab6858b3
mariacjones1 <>
2023-11-26 12:13:41 +0000

    Add follow functionality

--- frontend/src/pages/profiles/Profile.js
+++ frontend/src/pages/profiles/Profile.js
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Avatar from '../../components/Avatar';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Button } from 'react-bootstrap';
+import { useSetProfileData } from '../../contexts/ProfileDataContext';
 
 const Profile = (props) => {
     const { profile, mobile, imageSize = 55 } = props;
@@ -12,6 +13,8 @@ const Profile = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+
+    const { handleFollow } = useSetProfileData();
 
     return (
         <div className={`my-3 d-flex align-items-center ${mobile && "flex-column"}`}>
@@ -35,7 +38,7 @@ const Profile = (props) => {
                     ) : (
                         <Button
                             className={`${btnStyles.Button} ${btnStyles.Navy}`}
-                            onClick={() => { }}
+                            onClick={() => handleFollow(profile)}
                         >
                             follow
                         </Button>
