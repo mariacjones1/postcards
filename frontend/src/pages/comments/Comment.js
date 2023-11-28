c5ca34c0fd43f0732a7d09b0ac347863f57b0cc3
mariacjones1 <>
2023-11-28 18:26:13 +0000

    Add functionality to delete comments

--- frontend/src/pages/comments/Comment.js
+++ frontend/src/pages/comments/Comment.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Col, Media, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { axiosRes } from '../../api/axiosDefaults';
 import Avatar from '../../components/Avatar';
+import { MoreDropdown } from '../../components/MoreDropdown';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import styles from '../../styles/Comment.module.css';
 import postStyles from "../../styles/Post.module.css";
@@ -14,11 +16,33 @@ const Comment = (props) => {
         updated_at,
         content,
         comment_likes_count,
-        comment_like_id
+        comment_like_id,
+        id,
+        setPost,
+        setComments,
     } = props;
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner;
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/comments/${id}`);
+            setPost(prevPost => ({
+                results: [{
+                    ...prevPost.results[0],
+                    comments_count: prevPost.results[0].comments_count - 1
+                }]
+            }));
+
+            setComments(prevComments => ({
+                prevComments,
+                results: prevComments.results.filter((comment) => comment.id !== id),
+            }));
+        } catch (err) {
+            console.log(err);
+        }
+    };
 
     return (
         <div>
@@ -37,24 +61,31 @@ const Comment = (props) => {
                     </Row>
                 </Media.Body>
                 <Col xs={2} className={styles.Like}>
-                    {is_owner ? (
-                        <OverlayTrigger placement="top" overlay={<Tooltip className={postStyles.NoWrap}>You can't like your own comment :&#40;</Tooltip>}>
-                            <i className="far fa-heart" />
-                        </OverlayTrigger>
-                    ) : comment_like_id ? (
-                        <span onClick={() => { }}>
-                            <i className={`fas fa-heart ${postStyles.Heart}`} />
-                        </span>
-                    ) : currentUser ? (
-                        <span onClick={() => { }}>
-                            <i className={`far fa-heart ${postStyles.HeartOutline}`} />
-                        </span>
-                    ) : (
-                        <OverlayTrigger placement="top" overlay={<Tooltip className={postStyles.NoWrap}>Log in to like comments :&#41;</Tooltip>}>
-                            <i className="far fa-heart" />
-                        </OverlayTrigger>
-                    )}
-                    {comment_likes_count}
+                    <div className={styles.Dropdown}>
+                        {is_owner && (
+                            <MoreDropdown handleEdit={() => { }} handleDelete={handleDelete} />
+                        )}
+                    </div>
+                    <div className={postStyles.NoWrap}>
+                        {is_owner ? (
+                            <OverlayTrigger placement="top" overlay={<Tooltip>You can't like your own comment :&#40;</Tooltip>}>
+                                <i className="far fa-heart" />
+                            </OverlayTrigger>
+                        ) : comment_like_id ? (
+                            <span onClick={() => { }}>
+                                <i className={`fas fa-heart ${postStyles.Heart}`} />
+                            </span>
+                        ) : currentUser ? (
+                            <span onClick={() => { }}>
+                                <i className={`far fa-heart ${postStyles.HeartOutline}`} />
+                            </span>
+                        ) : (
+                            <OverlayTrigger placement="top" overlay={<Tooltip>Log in to like comments :&#41;</Tooltip>}>
+                                <i className="far fa-heart" />
+                            </OverlayTrigger>
+                        )}
+                        {comment_likes_count}
+                    </div>
                 </Col>
             </Media>
         </div>
