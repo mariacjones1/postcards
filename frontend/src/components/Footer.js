b50c563beba26be44de738c6d4ee83faa64b378c
mariacjones1 <>
2023-11-28 21:04:39 +0000

    Style footer to align items vertically and add background to hide content behind it

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Nav } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
+import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
         <>
-            <Nav justify className="fixed-bottom">
+            <Nav justify className={`${styles.Footer} fixed-bottom`}>
                 <Nav.Item>
                     <Nav.Link
                         href="https://github.com/mariacjones1"
                         target="_blank"
+                        className={styles.Padding}
                     >
                         GitHub
                     </Nav.Link>
