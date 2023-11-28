c9e4b04b08a8be56bbc78917a5020d3390cc986e
mariacjones1 <>
2023-11-28 14:26:14 +0000

    Add comment create form to post page

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -10,10 +10,16 @@ import TrendingProfiles from '../profiles/TrendingProfiles';
 import { useParams } from 'react-router-dom/cjs/react-router-dom.min';
 import { axiosReq } from '../../api/axiosDefaults';
 import Post from './Post';
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function PostPage() {
     const { id } = useParams();
     const [post, setPost] = useState({ results: [] });
+
+    const currentUser = useCurrentUser();
+    const profile_image = currentUser?.profile_image;
+    const [comments, setComments] = useState({ results: [] });
 
     useEffect(() => {
         const handleMount = async () => {
@@ -37,7 +43,17 @@ function PostPage() {
                 <PopularProfiles mobile />
                 <Post {...post.results[0]} setPosts={setPost} postPage />
                 <Container className={appStyles.Content}>
-                    Comments
+                    {currentUser ? (
+                        <CommentCreateForm
+                            profile_id={currentUser.profile_id}
+                            profileImage={profile_image}
+                            post={id}
+                            setPost={setPost}
+                            setComments={setComments}
+                        />
+                    ) : comments.results.length ? (
+                        "Comments"
+                    ) : null}
                 </Container>
             </Col>
             <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
